14d4636327f047bda18ae3b70a5df368bb331959
Ken Britton <kenbritton@gmail.com>
2016-02-01 21:14:44 -0800

    move launchd job to run on user login

--- launchctl/reload.sh
+++ launchctl/reload.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-launchctl unload com.kenbritton.check_campsites.plist
-launchctl load com.kenbritton.check_campsites.plist
+
+PLIST="com.kenbritton.check_campsites.plist"
+USER_AGENT_DIR="$HOME/Library/LaunchAgents/"
+USER_AGENT_PLIST="$USER_AGENT_DIR/$PLIST"
+
+cp $PLIST $USER_AGENT_DIR
+launchctl unload $USER_AGENT_PLIST
+launchctl load $USER_AGENT_PLIST
